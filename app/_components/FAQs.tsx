966d77948832e82355dd43ef4b49fbf540e6064c
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2025-01-30 13:30:08 +0100

    fix:fix faq border and background

--- app/_components/FAQs.tsx
+++ app/_components/FAQs.tsx
@@ -16,7 +16,7 @@ const FAQs = () => {
       id='faqs'
       className={`${halant.className} px-8 flex py-40 bg-neutral-50 text-black`}
     >
-      <div className='relative md:w-3/5 border-[1px] mx-auto border-primary-700 bg-white rounded-md p-6  before:absolute before:content-[""] before:rounded-md before:w-full before:h-full before:border-[1px] before:border-primary-700 before:bottom-2 before:left-2 before:-z-[2] z-[2] '>
+      <div className='relative md:w-3/5 border-[1px] mx-auto border-primary-700 bg-white rounded-md p-6  before:absolute before:content-[""] before:bg-white before:rounded-md before:w-full before:h-full before:border-[1px] before:border-primary-700 before:top-2 before:right-2 before:-z-[2] z-[2] '>
         <header className='flex flex-col gap-4 justify-center items-center'>
           <Image src={Pin} quality={70} alt='pin' />
           <h1 className='text-center font-semibold text-3xl leading-10'>
